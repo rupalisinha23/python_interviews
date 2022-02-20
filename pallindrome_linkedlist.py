e0adb7fa201a061e332741f460dacca04e619253
Supratim Das <supratim.gre2014@yahoo.com>
2022-02-20 11:40:47 +0100

    Create pallindrome_linkedlist.py

---
+++ pallindrome_linkedlist.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        if not head:
+            return True
+
+        fast = head
+        slow = head
+        prev = None
+        flag = 1
+
+        while fast and fast.next:
+            if not fast.next.next:
+                flag = 0
+                break
+            fast = fast.next.next
+            temp = slow
+            slow = slow.next
+            temp.next = prev
+            prev = temp
+
+        fast = slow.next
+        slow.next = prev
+
+        if flag:
+            slow = slow.next
+      
+        while fast and slow:
+            if fast.val != slow.val:
+                return False
+            fast = fast.next
+            slow = slow.next
+
+        return True
+            
+        

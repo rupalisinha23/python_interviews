5f5c193a7fa5fc299acb073711195efae297cf6b
Supratim Das <supratim.gre2014@yahoo.com>
2022-02-24 15:13:35 +0100

    Create balanced_or_not.py

---
+++ Tree/balanced_or_not.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def height(self, root):
+        if None == root:
+            return 0
+        l = self.height(root.left)
+        r = self.height(root.right)
+        return max(l, r) + 1
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        lb = self.isBalanced(root.left)
+        rb = self.isBalanced(root.right)
+    
+        diff = abs(self.height(root.left) - self.height(root.right)) <= 1
+    
+        if lb & rb & diff:
+            return True
+        else:
+            return False
+        
